eaa1ce9b3148eb70dc430aa65f05de61e55c0235
welz-atm <59687594+welz-atm@users.noreply.github.com>
2020-04-15 15:50:01 +0100

    Update

--- covid19/settings.py
+++ covid19/settings.py
@@ -99,24 +99,24 @@ DEBUG = config('DEBUG',default=False ,cast=bool)
 
 
 
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': config('DB_NAME'),
+        'USER': config('DB_USER'),
+        'PASSWORD': config('DB_PASSWORD'),
+        'HOST': config('DB_HOST'),
+        'PORT': '',
+    }
+}
 
 
 #DATABASES = {
-#    'default': {
-#        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-#        'NAME': config('DB_NAME'),
-#        'USER': config('DB_USER'),
-#        'PASSWORD': config('DB_PASSWORD'),
-#        'HOST': config('DB_HOST'),
-#        'PORT': '',
-#    }
+#    'default': dj_database_url.config(
+#        default=config('DATABASE_URL')
+#    )
 #}
 
-DATABASES = {
-    'default': dj_database_url.config(
-        default=config('DATABASE_URL')
-    )
-}
 
 
 
