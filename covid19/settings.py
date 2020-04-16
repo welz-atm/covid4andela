f5399cea53d65a77dfcd0d4d14e1834a7d093add
welz-atm <59687594+welz-atm@users.noreply.github.com>
2020-04-16 20:08:21 +0100

    Readjustment

--- covid19/settings.py
+++ covid19/settings.py
@@ -157,7 +157,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 PROJECT_ROOT = os.path.join(os.path.abspath(__file__))
-STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (
     os.path.join(PROJECT_ROOT, 'static'),
